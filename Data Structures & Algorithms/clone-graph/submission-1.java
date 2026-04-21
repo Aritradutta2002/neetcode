0bfaa0b3d9a855176c78e6c0a658913536a50197
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-04-22 00:59:51 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.java
@@ -0,0 +1,41 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        Queue<Node> q = new ArrayDeque<>();
+        Map<Node, Node> newMap = new HashMap<>();
+        if (node == null)
+            return null;
+        newMap.put(node, new Node(node.val));
+        q.offer(node);
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+            for (Node n : curr.neighbors) {
+                if (!newMap.containsKey(n)) {
+                    q.add(n);
+                    newMap.put(n, new Node(n.val));
+                }
+                newMap.get(curr).neighbors.add(newMap.get(n));
+            }
+        }
+        return newMap.get(node);
+    }
+}
